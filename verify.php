b093c76030ad31cb0914e1abebfa3b2ac2506cfc
markLabel9822 <45809781+markLabel9822@users.noreply.github.com>
2018-12-12 13:38:58 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'YA/poc0Jd62ZyrKs2fsZSLVhVb8MWIyflVKuahqFh1Jo4tqFf0X7bCQ/r5r3D4SGQ5gWJBYUrKUUyBw/hhh3iZ2+y8Rt08/MHKbWTAlAiP5ld2MCW9ctjUs9mUWkM3Fkj1wnhhrHS+K3ChcJqz24MgdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
